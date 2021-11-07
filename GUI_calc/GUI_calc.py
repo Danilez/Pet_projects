a4f77d8853e0b5351fc81bb4967b22fe393a9adb
Danilez <ooochen_zlaya@mail.ru>
2021-11-08 01:39:03 +0300

    making functions

---
+++ GUI_calc/GUI_calc.py
@@ -0,0 +1,48 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from design import Ui_MainWindow
+
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super(Calculator, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # digits
+        self.ui.btn_0.clicked.connect(self.add_digit)
+        self.ui.btn_1.clicked.connect(self.add_digit)
+        self.ui.btn_2.clicked.connect(self.add_digit)
+        self.ui.btn_3.clicked.connect(self.add_digit)
+        self.ui.btn_4.clicked.connect(self.add_digit)
+        self.ui.btn_5.clicked.connect(self.add_digit)
+        self.ui.btn_6.clicked.connect(self.add_digit)
+        self.ui.btn_7.clicked.connect(self.add_digit)
+        self.ui.btn_8.clicked.connect(self.add_digit)
+        self.ui.btn_9.clicked.connect(self.add_digit)
+
+    def add_digit(self):
+        btn = self.sender()
+
+        digit_buttons = ('btn_0', 'btn_1', 'btn_2', 'btn_3', 'btn_4', 'btn_5', 'btn_6', 'btn_7', 'btn_8', 'btn_9')
+
+        if btn.objectName() in digit_buttons:
+            if self.ui.lineEdit.text() == '0':
+                self.ui.lineEdit.setText(btn.text())
+            else:
+                self.ui.lineEdit.setText(self.ui.lineEdit.text() + btn.text())
+
+    def sender(self):  # real signature unknown; restored from __doc__
+        """sender(self) -> PySide6.QtCore.QObject"""
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Calculator()
+    window.show()
+
+    sys.exit(app.exec())
